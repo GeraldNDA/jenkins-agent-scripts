c2530fadfe5a7f1b41cf15e8f8412870e2a035cf
Gerald Aryeetey <gndaryee@uwaterloo.ca>
2019-03-14 19:25:16 -0400

    Improve agent scripts
    - Use jenkins_url instead of master, since you may want to use http:// instead of https://
    - Make ipv6 optional

--- startagent.sh
+++ startagent.sh
@@ -20,6 +20,11 @@ if [ -z "${agentname}" ]; then
 	agentname=`/bin/hostname`
 fi
 
+ipv6opt=""
+if [ "${use_ipv6}" = "YES" ]; then
+	ipv6opt="-Djava.net.preferIPv6Addresses=true"
+fi
+
 if [ -n "${nice_increment}" ] && [ ${nice_increment} -ne 0 ]; then
 	NICE_CMD="/usr/bin/nice -n ${nice_increment}"
 fi
@@ -28,10 +33,10 @@ while [ ! -f agent.dontstart ]
 do
 	/bin/date
 	# mirror mode, update it if there's a timestamp change on the master
-	/usr/bin/fetch -m -o agent.jar "https://${master}/jnlpJars/agent.jar"
-	${NICE_CMD} /usr/local/bin/java -Djava.net.preferIPv6Addresses=true \
+	/usr/bin/fetch -m -o agent.jar "${jenkins_url}/jnlpJars/agent.jar"
+	${NICE_CMD} /usr/local/bin/java ${ipv6opt} \
 		-jar agent.jar \
-		-jnlpUrl "https://${master}/computer/${agentname}/slave-agent.jnlp" \
+		-jnlpUrl "${jenkins_url}/computer/${agentname}/slave-agent.jnlp" \
 		-secret "${secret}"
 	/bin/sleep 30
 done
